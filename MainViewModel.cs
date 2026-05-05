421a9d38670c1e37ce9164cd4059c57e85c6ce90
mohit <mohitgiri1103@gmail.com>
2026-05-06 02:15:38 +0530

    Detect Running Apps

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -1,21 +1,40 @@
 using System.ComponentModel;
+using System.Collections.ObjectModel;
+using SecureAppLocker.Services;
 
 namespace SecureAppLocker.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+        public readonly ProcessService _processService;
+        public ObservableCollection<string> RunningApps { get; set; }
 
-        private string _status = "App Ready";
-
-        public string Status
+        public MainViewModel()
         {
-            get => _status;
-            set
+            _processService = new ProcessService();
+            RunningApps = new ObservableCollection<string>();
+            LoadProcesses();
+        }
+
+        public void LoadProcesses()
+        {
+            var processes = _processService.GetRunningProcess();
+            RunningApps.Clear();
+            foreach(var process in processes)
             {
-                _status = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Status)));
+                RunningApps.Add(process);   
             }
         }
+        //private string _status = "App Ready";
+        //public string Status
+        //{
+        //    get => _status;
+        //    set
+        //    {
+        //        _status = value;
+        //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Status)));
+        //    }
+        //}
     }
 }
